356e30b553559c35fc0d14d5a42749eded835b04
zzhhbs <908924736@qq.com>
2020-08-11 08:49:55 +0800

    本地hello1

---
+++ hellogit/src/main/java/cn/kgc/HelloWorld.java
@@ -0,0 +1,11 @@
+package cn.kgc;
+
+/**
+ * Created by helloworld on 2020/8/11.
+ * 暂存区
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
